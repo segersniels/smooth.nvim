d7bb0f25c6b0a9df5a62d4f82460a9f80d789dfd
segersniels <segers.n@hotmail.com>
2025-01-17 16:43:05 +0100

    feat: implement light theme (experimental)

--- lua/smooth/integrations/render-markdown.lua
+++ lua/smooth/integrations/render-markdown.lua
@@ -2,16 +2,16 @@ local M = {}
 
 function M.get()
 	return {
-		RenderMarkdownCode = { bg = C.darkgray },
-		RenderMarkdownCodeInline = { bg = C.darkgray },
-		RenderMarkdownBullet = { fg = C.blue },
-		RenderMarkdownTableHead = { fg = C.blue },
-		RenderMarkdownTableRow = { fg = C.magenta },
-		RenderMarkdownSuccess = { fg = C.green },
-		RenderMarkdownInfo = { fg = C.blue },
-		RenderMarkdownHint = { fg = C.cyan },
-		RenderMarkdownWarn = { fg = C.yellow },
-		RenderMarkdownError = { fg = C.red },
+		RenderMarkdownCode = { bg = C.current.darkgray },
+		RenderMarkdownCodeInline = { bg = C.current.darkgray },
+		RenderMarkdownBullet = { fg = C.current.blue },
+		RenderMarkdownTableHead = { fg = C.current.blue },
+		RenderMarkdownTableRow = { fg = C.current.magenta },
+		RenderMarkdownSuccess = { fg = C.current.green },
+		RenderMarkdownInfo = { fg = C.current.blue },
+		RenderMarkdownHint = { fg = C.current.cyan },
+		RenderMarkdownWarn = { fg = C.current.yellow },
+		RenderMarkdownError = { fg = C.current.red },
 	}
 end
 
